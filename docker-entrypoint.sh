6de42708d7706e362fc57202658f1f33964d74fb
Gabriel Britain <quontas@gmail.com>
2019-04-08 00:32:15 -0500

    Altered docker-entrypoint.sh to reduce amount of commands needed.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,8 @@ python3 manage.py makemigrations --settings=goodbullschedules.settings.docker
 echo "Migrating"
 python3 manage.py migrate --settings=goodbullschedules.settings.docker --run-syncdb
 
+echo "Making PDF Directory"
+mkdir -p goodbullschedules/documents/pdfs
+
 echo "Starting"
 python3 manage.py runserver --settings=goodbullschedules.settings.docker 0.0.0.0:8000
