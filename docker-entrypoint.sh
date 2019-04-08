6358e9c821096602a95feb567f8b6ce8aabb7898
Gabriel Britain <quontas@gmail.com>
2019-04-08 09:45:32 -0500

    Added initialization scripts.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ echo "Migrating"
 python3 manage.py migrate --settings=goodbullschedules.settings.docker --run-syncdb
 
 echo "Making PDF Directory"
-mkdir -p goodbullschedules/documents/pdfs
+mkdir -p /app/documents/pdfs
 
 echo "Starting"
 python3 manage.py runserver --settings=goodbullschedules.settings.docker 0.0.0.0:8000
